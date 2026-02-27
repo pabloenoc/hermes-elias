873c8d76b84d3fa74a6da3b096f85160f81d345d
STORMTR00P3R <noorblum16@gmail.com>
2026-02-26 17:34:27 -0800

    linkpost styles

--- public/index.php
+++ public/index.php
@@ -13,70 +13,69 @@ while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
 $errors = [];
 
 foreach($feeds as $index => $feed) {
-	try {
-		$xml = Feed::load($feed['url']);
+    try {
+	$xml = Feed::load($feed['url']);
         $feeds[$index]['xml'] = $xml;
-	}
-	catch(FeedException | Exception $e) {
+    }
+    catch(FeedException | Exception $e) {
         $errors[] = get_class($e) . ": " . $e->getMessage() . " - " . $feed['url'];
-	}
+    }
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Hermes - Elias</title>
-</head>
-<body>
-    <navbar>
-        <a href='/'>
-            <img src='images/logo.webp'>
-            <h1>Home</h1>
-        </a>
-    </navbar>
-    <main>
-        <div>
-            <?php require __DIR__ . '/new_feed.php' ?>
-        </div>
+    <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>Hermes - Elias</title>
+    </head>
+    <body>
+	<navbar>
+            <a href='/'>
+		<img src='images/logo.webp'>
+		<h1>Home</h1>
+            </a>
+	</navbar>
+	<main>
+            <div>
+		<?php require __DIR__ . '/new_feed.php' ?>
+            </div>
 
-        <div class="page-errors">
-            <h2>Error(s)</h2>
+            <div class="page-errors">
+		<h2>Error(s)</h2>
 
-            <?php if (count($errors) > 0): ?>
-                <ul>
-                    <?php foreach($errors as $error): ?>
-                    <li><?= $error ?></li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
-            
-        </div>
+		<?php if (count($errors) > 0): ?>
+                    <ul>
+			<?php foreach($errors as $error): ?>
+			    <li><?= $error ?></li>
+			<?php endforeach; ?>
+                    </ul>
+		<?php endif; ?>            
+            </div>
 
-        <div>
+            <div>
         	<?php foreach ($feeds as $index => $feed): ?>
-                <p><?= $feed['title'] ?></p>
-                <ul>                	
-                	<?php if (isset($feed['xml'])): ?>
-                		<?php foreach ($feed['xml']->item as $item): ?>
-                			<li>
-                				<a href="<?= $item->link ?>" target="_blank">
-                				<?= $item->title ?>
-                				</a>
-                			</li>
-                		<?php endforeach; ?>
-                	<?php else: ?>
-                		<li>No items found.</li>	
-                	<?php endif; ?>
-                </ul>
-            <?php  endforeach; ?>
-        </div>
-    </main>
+                    <p><?= $feed['title'] ?></p>
 
-</body>
+                	<?php if (isset($feed['xml'])): ?>
+                	    <?php foreach ($feed['xml']->item as $item): ?>
+                		<div class="linkpost">
+                		    <a href="<?= $item->link ?>" target="_blank">
+                			<?= $item->title ?>
+                		    </a>
+                		</div>
+                	    <?php endforeach; ?>
+                	<?php else: ?>
+                	    <div style="opacity: 0.5;">No items found.</div>	
+                	<?php endif; ?>
+
+		<?php  endforeach; ?>
+            </div>
+	</main>
+
+    </body>
 </html>
 
