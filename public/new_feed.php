5f73ef2e09c89eb8343c35eea247ad6ab8a901de
STORMTR00P3R <83847724+STORMTR00P3R@users.noreply.github.com>
2026-03-01 17:49:06 -0800

    begin new feed script

--- public/new_feed.php
+++ public/new_feed.php
@@ -1,8 +1,13 @@
 <?php
 
-// Connect to our database
+$db = new SQLite3(__DIR__ . '/../db/hermes_development.sqlite');
+
+if($_SERVER['REQUESTED_METHOD'] === 'POST' && isset($_POST['new_feed.url'])) {
+	echo "you sumbitted feed URL" . $_POST['new_feed/url']
+}
 
 ?>
+
 <form method="post">
 	<input type="url" placeholder="https://example.com">
 	<input type="submit" value="+">
