762c2378a6b151da39d7b7543596ea47bba93906
STORMTR00P3R <83847724+STORMTR00P3R@users.noreply.github.com>
2026-03-01 17:54:01 -0800

    revision

--- public/new_feed.php
+++ public/new_feed.php
@@ -2,8 +2,8 @@
 
 $db = new SQLite3(__DIR__ . '/../db/hermes_development.sqlite');
 
-if($_SERVER['REQUESTED_METHOD'] === 'POST' && isset($_POST['new_feed.url'])) {
-	echo "you sumbitted feed URL" . $_POST['new_feed/url']
+if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['new_feed_url'])) {
+	echo "you sumbitted feed URL" . $_POST['new_feed_url']
 }
 
 ?>
