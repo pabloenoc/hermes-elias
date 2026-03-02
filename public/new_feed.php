86bdc53b9d1596fc1ab2732247c7f23044c837ba
STORMTR00P3R <83847724+STORMTR00P3R@users.noreply.github.com>
2026-03-01 17:59:04 -0800

    add semicolon

--- public/new_feed.php
+++ public/new_feed.php
@@ -3,7 +3,7 @@
 $db = new SQLite3(__DIR__ . '/../db/hermes_development.sqlite');
 
 if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['new_feed_url'])) {
-	echo "you sumbitted feed URL" . $_POST['new_feed_url']
+	echo "you sumbitted feed URL" . $_POST['new_feed_url'];
 }
 
 ?>
